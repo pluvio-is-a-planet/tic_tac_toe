1f6e62d38071811e585b2a604dfe7f59d3f1f8fd
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-09 14:55:14 +0200

    feat: add play method
    Added the play method that takes input from the command line, to
    determine at what position the player wants to make their move. Then
    the current_turn instance variable gets swapped.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -16,19 +16,47 @@
 # play method
 # check winner method
 class GameBoard
-  attr_accessor :board
+  attr_accessor :board, :game_is_active, :current_turn, :last_turn
 
   def initialize
-    @board = Array.new(3, Array.new(3, "-"))
+    @board = Array.new(3) { Array.new(3, "-") }
+    @game_is_active = true
+
+    puts "Which player starts first? 'X' or 'O'"
+    starting_turn = gets.chomp.upcase
+    @current_turn = starting_turn
+    @last_turn = (current_turn == "X") ? "O" : "X"
+
     print_board
   end
 
   def print_board
     puts "   123"
     board.each_with_index do |row, index|
-      str = String.new("#{(index + 65).chr}: ")
+      str = "#{(index + 65).chr}: "
       row.each { |col| str += col}
       puts str
     end
+  end
+
+  def play
+    puts "Enter position to make your move (Eg. 'a2')>>"
+
+    position = gets.chomp
+    row_pos = position[0].upcase.ord - 65
+    col_pos = position[1].to_i - 1
+
+    if board[row_pos][col_pos] == "-"
+      board[row_pos][col_pos] = current_turn
+      print_board
+
+      temp = self.last_turn
+      self.last_turn = self.current_turn
+      self.current_turn = temp
+      # check_win
+    else
+      puts "You cannot play here, position already taken."
+    end
+
   end
 end
