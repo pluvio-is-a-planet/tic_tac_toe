905b19d5d3dd1880373dcd9d096bf58af7907890
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-09 18:06:20 +0200

    refactor: remove psuedo-code comments

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,20 +1,3 @@
-# board:
-#   □□□
-#   □□□
-#   □□□
-
-# first play is "X"
-# check the last player to determine the next player
-# if either player gets 3 squares in a row, column, or diagonal line, they win
-#   get the position of where to place the shape
-#   check if there's a winner at the end of every move made
-
-# board class
-# init array of arrays with positions
-# board instance var
-# current play instance var
-# play method
-# check winner method
 class GameBoard
   attr_accessor :board, :game_is_active, :current_turn, :last_turn
 
