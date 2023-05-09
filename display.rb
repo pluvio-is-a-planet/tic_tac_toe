f69248bcd53b67ee18f2ea1db70a742fe1237665
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-09 21:19:57 +0200

    refactor: update print_board method
    Added the update_board method instead of directly updating the board
    from the play_turn method within the TicTacToe module.
    
    Then made sure that the terminal output gets cleared every time the
    board gets printed to the terminal, so that the output does not get
    clogged up.

--- display.rb
+++ display.rb
@@ -1,12 +1,25 @@
+# frozen_string_literal: false
+
+require './logic'
+require 'io/console'
+
 module Display
+  include TicTacToe
+
   def print_board
-    puts '\n   1 2 3'
+    $stdout.clear_screen
+    puts "\n   1 2 3"
     board.each_with_index do |row, index|
       row_str = "#{(index + 65).chr}: " # Get the alphabetical representation of the index
       row_pos_str = ''
-      row.each { |col| row_pos_str += col}
+      row.each { |col| row_pos_str += col }
       row_pos_str = row_pos_str.split('').join(' ') # Add a space between all the available positions
-      puts row_str += row_pos_str
+      puts row_str + row_pos_str
     end
+  end
+
+  def update_board(row, col)
+    board[row][col] = current_turn
+    print_board
   end
 end
