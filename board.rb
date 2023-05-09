b59312abe2432b555aff1d1003efb5336bff7949
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-09 22:06:51 +0200

    fix: ensure the user enters a valid starting_turn
    Made it so that until the first character of the user's input is equal
    to either 'X' or 'O', the user gets prompted for the starting_turn.

--- board.rb
+++ board.rb
@@ -13,8 +13,11 @@ class Board
     @board = Array.new(3) { Array.new(3, '-') }
     @game_is_active = true
 
-    puts "Which player starts first? 'X' or 'O'"
-    starting_turn = gets.chomp.upcase
+    starting_turn = ''
+    until %w[X O].include? starting_turn[0]
+      puts "Which player starts first? 'X' or 'O'"
+      starting_turn = gets.chomp[0].upcase
+    end
     @current_turn = starting_turn
     @last_turn = current_turn == 'X' ? 'O' : 'X'
 
